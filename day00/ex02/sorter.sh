1a14ea9d3b9fab228471c4f5678134d1042911b7
xdarov <xdarovv@gmail.com>
2022-09-17 17:49:51 +0300

    back 00|02

--- day00/ex02/sorter.sh
+++ day00/ex02/sorter.sh
@@ -8,4 +8,4 @@ if [ -z "$1" ]
 fi
 
 cat $FILE | head -n 1 > hh_sorted.csv &&
-cat $FILE | tail -n +2 | sort  >> hh_sorted.csv
+cat $FILE | tail -n +2 | sort --field-separator=',' --key=2,2 --key=1,1 >> hh_sorted.csv
