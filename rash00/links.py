d626d7c9b6548f16ccc53b418a47a6d163e4e254
Waso-python <waso.82@gmail.com>
2022-09-22 21:03:14 +0300

    begin the rush

--- rash00/links.py
+++ rash00/links.py
@@ -1,4 +1,4 @@
-# os, sys, urllib, requests, beautifulsoup, json, pytest, collections, functools, datetime, re
+import os, sys, urllib, requests, beautifulsoup, json, pytest, collections, functools, datetime, re
 
 class Links:
     """
@@ -8,7 +8,21 @@ class Links:
         """
         Put here any fields that you think you will need.
         """
-    
+        self.path_to_the_file = path_to_the_file
+        
+
+    def get_file_data(self):
+        result = []
+        try:
+            with open(self.path_to_the_file, 'r') as f:
+                for line in f:
+                    yield line
+        except (OSError, ValueError) as e:
+            print(f"Error reading file - {e}")
+        return 
+
+
+    @staticmethod
     def get_imdb(list_of_movies, list_of_fields):
         """
 The method returns a list of lists [movieId, field1, field2, field3, ...] for the list of movies given as the argument (movieId).
@@ -16,9 +30,18 @@ The method returns a list of lists [movieId, field1, field2, field3, ...] for th
         The values should be parsed from the IMDB webpages of the movies.
      Sort it by movieId descendingly.
         """
+        for line in list_of_movies:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            fields = line.split(',')
+            if len(fields) == 0:
+                continue
+
         return imdb_info
         
     def top_directors(self, n):
+        directors = self.get_imdb(self.get_file_data(), ['Director'])
         """
         The method returns a dict with top-n directors where the keys are directors and 
         the values are numbers of movies created by them. Sort it by numbers descendingly.
