27e34aa5e5a73896e87d339c6e962e50f225542d
xdarov <xdarovv@gmail.com>
2022-09-22 21:05:34 +0300

    start rush00

--- rash00/movies.py
+++ rash00/movies.py
@@ -1,3 +1,6 @@
+import os, sys, urllib, requests, json, pytest, collections, functools, datetime, re
+
+
 class Movies:
     """
     Analyzing data from movies.csv
@@ -5,12 +8,31 @@ class Movies:
     def __init__(self, path_to_the_file):
         """
         Put here any fields that you think you will need.
-        """    
+        """
+        self.file_path = path_to_the_file
+
+    def get_generator_file(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                for row in file:
+                    yield row
+        except OSError as e:
+            print(f'ERROR: {e}')
+
+
     def dist_by_release(self):
         """
         The method returns a dict or an OrderedDict where the keys are years and the values are counts. 
         You need to extract years from the titles. Sort it by counts descendingly.
         """
+        release_years = {}
+        for row in self.get_generator_file():
+            year = re.search(r'(\d\d\d\d)', row)[0]
+            if year in release_years:
+                release_years[year] += 1
+            else:
+                release_years[year] = 0
+            pass
         return release_years
     
     def dist_by_genres(self):
@@ -26,3 +48,7 @@ class Movies:
         the values are the number of genres of the movie. Sort it by numbers descendingly.
         """
         return movies
+
+
+# for i in Movies('ml-latest-small/movies.csv').get_generator_file():
+#     print(i)
